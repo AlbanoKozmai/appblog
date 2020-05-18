081c6fae852bbd4eea386586f2d816be0ba08395
AlbanoKozmai <albano.kozmai@yahoo.com>
2020-05-19 01:18:21 +0200

    Update login

--- config/config.php
+++ config/config.php
@@ -1,8 +1,15 @@
 <?php
+	session_start();
+
+	// Database Configuration
 	define('DB_HOST', 'localhost');
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', '');	
 	define('DB_DATABASE', 'blogapp');
 	$db = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
+	// Check database for error
+	if (!$db) {
+		die('Database connect filed: '.mysql_connect_error());
+	}
 ?>
--- login.php
+++ login.php
@@ -1,27 +1,54 @@
 <?php
 	require_once("config/config.php");
-	session_start();
 
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		$myusername = mysql_real_escape_string($db,$_POST['username']);
-		$mypassword = mysql_real_escape_string($db,$_POST['password']);
+	if(isset($_POST['submit_login'])) {
+		$myusername = mysqli_real_escape_string($db,$_POST['username']);
+		$mypassword = mysqli_real_escape_string($db,$_POST['password']);
+		
+		if ($myusername != "" && $mypassword != "") {
+			$sql_query = "select count(*) as cntUser from users where username='".$myusername."' and password='".$mypassword."'";
+			$result = mysqli_query($db,$sql_query);
+			$row = mysqli_fetch_array($result);
 
-		$sql = "SELECT id FROM admin WHERE username = '$myusername' and password = '$password'";
-		$result = mysqli_query($db,$sql);
-		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		$active = $row['active'];
+			$count = $row['cntUser'];
 
-		$count = mysqli_num_rows($result);
-
-		if($count == 1) {
-			session_register("myusername");
-			$_SESSION['login_user'] = $myusername;
-			
-			header("location: index.php");
-		} else {
-			$error = "Your name or passwor is invalid";
+			if($count > 0) {
+				$_SESSION['myusername'] = $myusername;
+				
+				header("location: index.php");
+			} else {
+				$error = "Your name or passwor is invalid";
+			}
 		}
 	}
+
+	// if(isset($_POST['username'])){
+		//     $myusername = $_POST['username'];
+		//     $myusername = stripcslashes($myusername);		
+		// 	$myusername = mysql_real_escape_string($myusername);
+		// }
+		// if(isset($_POST['password'])){
+		//     $mypassword  = $_POST['usernamepassword'];
+		//     $mypassword = stripcslashes($mypassword);
+		//     $mypassword = mysql_real_escape_string($mypassword);
+		// }
+
+		
+		
+
+		// mysql_connect("localhost", "root", "");
+		// mysql_select_db("blogapp");
+
+		// $result = mysql_query("select * from users where username = '$myusername' and password = '$mypassword'") or die("Failed to query database ".mysql_error());
+		// $row = mysql_fetch_array($result);
+
+		// if ($row['username'] == $myusername && $row['password'] == $mypassword ) {
+		// 	header("location: index.php");
+		// }else {
+		// 	echo "Filet to login";
+		// }
+
+
 
 	$title = 'Log In';
 	$description = 'This is the login page';
@@ -32,20 +59,20 @@
 		<div class="col-12 align-self-center">
 			<div class="login-page">
 			  <div class="form">
-			  	<form id="login" class="login-form" action="post"> 
+			  	<form id="login" class="login-form" method="post" > 
 			      <input type="text" name="username" placeholder="username"/>
 			      <input type="password" name="password" placeholder="password"/>
-			      <button>login</button>
+			      <button type="submit" name="submit_login">login</button>
 			      <p class="message">Not registered? <a href="#">Create an account</a></p>
 			    </form>
-			    <form id="register" class="register-form" action="post">
+			    <!-- <form id="register" class="register-form" action="post">
 			      <input type="text" placeholder="name"/>
 			      <input type="password" placeholder="password"/>
 			      <input type="text" placeholder="email address"/>
 			      <button>create</button>
 			      <p class="message">Already registered? <a href="#">Sign In</a></p>
 			    </form>
-			    
+			     -->
 			  </div>
 			</div>
 		</div>
@@ -53,5 +80,5 @@
 	
 
 <?php
-	include_once('footer.php');
+	require_once('footer.php');
 ?>
